71b8d122aa046ac54c30a8fd0ea1c1d7068fda11
eggtart26 <justin.guan26@gmail.com>
2023-01-16 18:32:57 -0500

    implemented mock api data

---
+++ src/api/mockApi.js
@@ -0,0 +1,90 @@
+export default function () {
+    return Promise.resolve([
+      {
+        cusid: 1,
+        amount: 120,
+        transactionDate: "01-01-2023",
+      },
+      {
+        cusid: 1,
+        amount: 75,
+        transactionDate: "01-03-2023",
+      },
+      {
+        cusid: 1,
+        amount: 94,
+        transactionDate: "01-22-2019",
+      },
+      {
+        cusid: 1,
+        amount: 10,
+        transactionDate: "02-01-2023",
+      },
+      {
+        cusid: 1,
+        amount: 75,
+        transactionDate: "02-20-2023",
+      },
+      {
+        cusid: 1,
+        amount: 200,
+        transactionDate: "03-02-2023",
+      },
+      {
+        cusid: 1,
+        amount: 190,
+        transactionDate: "03-15-2023",
+      },
+      {
+        cusid: 1,
+        amount: 80,
+        transactionDate: "03-16-2023",
+      },
+      {
+        cusid: 2,
+        amount: 125,
+        transactionDate: "01-01-2023",
+      },
+      {
+        cusid: 2,
+        amount: 75,
+        transactionDate: "01-22-2023",
+      },
+      {
+        cusid: 2,
+        amount: 10,
+        transactionDate: "02-01-2023",
+      },
+      {
+        cusid: 2,
+        amount: 75,
+        transactionDate: "02-06-2023",
+      },
+      {
+        cusid: 2,
+        amount: 200,
+        transactionDate: "03-05-2023",
+      },
+      {
+        cusid: 2,
+        amount: 224,
+        transactionDate: "03-28-2023",
+      },
+      {
+        cusid: 3,
+        amount: 100,
+        transactionDate: "01-02-2023",
+      },
+      {
+        cusid: 3,
+        amount: 90,
+        transactionDate: "02-02-2023",
+      },
+      {
+        cusid: 3,
+        amount: 88,
+        transactionDate: "03-02-2023",
+      },
+    ]);
+  }
+  
